57f94fb26cf62ea483f0596a16c6db08cf60795a
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-04-12 17:46:21 +0200

    Improvement in first test Add test method calculateJackardSimilarity Where Sequence Length Is Zero

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,13 +9,31 @@ import static org.hamcrest.Matchers.is;
 
 public class SimilarityFinderTest {
 
+    SimilarityFinder similarityFinder;
+
     @Test
     public void calculateJackardSimilarityTestWhereSequenceLengthIsZero() {
-        SimilarityFinder similarityFinder = new SimilarityFinder();
+        similarityFinder = new SimilarityFinder();
         int[] seq = {};
         int[] seq2 = {};
         double result = 1.0d;
 
-        Assert.assertThat(result, is(equalTo(similarityFinder.calculateJackardSimilarity(seq, seq2))));
+        Assert.assertThat("should be 1.0d", result, is(equalTo(similarityFinder.calculateJackardSimilarity(seq, seq2))));
     }
+
+    @Test
+    public void calculateJacardSimilarityTestWhereSequenceAreTheSameWithTheSameElements() {
+        int[] seq = {1, 2, 3, 4, 5};
+        int[] seq2 = {5, 4, 3, 2, 1};
+        double result = 1.0d;
+
+        similarityFinder = new SimilarityFinder((key, seq1) -> {
+            if (key == seq1[0] || key == seq1[1] || key == seq1[2] || key == seq1[3] || key == seq1[4])
+                return SearchResult.builder().withFound(true).build();
+            else return SearchResult.builder().withFound(false).build();
+        });
+
+        Assert.assertThat("should be 1.0", similarityFinder.calculateJackardSimilarity(seq, seq2), is(equalTo(result)));
+    }
+
 }
