4c643979dbb4086ab38cfa6483facc9013cacbd2
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-04-12 17:47:13 +0200

    Add test method calculateJackardSimilarity Where Sequence Are Different

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -36,4 +36,20 @@ public class SimilarityFinderTest {
         Assert.assertThat("should be 1.0", similarityFinder.calculateJackardSimilarity(seq, seq2), is(equalTo(result)));
     }
 
+    @Test
+    public void calculateJacardSimilarityTestWhereSequenceAreDifferent() {
+        int[] seq = {1, 2, 3, 4, 5};
+        int[] seq2 = {6, 7, 8, 9, 10};
+        double result = 0.0;
+
+        similarityFinder = new SimilarityFinder((key, seq1) -> {
+            if (key == seq1[0] || key == seq1[1] || key == seq1[2] || key == seq1[3] || key == seq1[4])
+                return SearchResult.builder().withFound(true).build();
+            else return SearchResult.builder().withFound(false).build();
+        });
+
+        Assert.assertThat("should be 0.0", similarityFinder.calculateJackardSimilarity(seq, seq2), is(equalTo(result)));
+    }
+
+
 }
